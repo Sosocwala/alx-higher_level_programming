4f96f676d9c58b18deebc3722591735eceff089a
Sonwabile <sonwabilecwala6@gmail.com>
2023-03-08 16:20:23 -0800

    python task

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,5 @@
-#/user/bin/bash/python
-def islower(c)
+#!/usr/bin/python3
+def islower(c):
     if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,4 @@
-#!/uer/bin/bash/python3
+#!/usr/bin/python3
 def print_last_digit(number):
     '''prints the last digit of a number'''
     last_digit = abs(number) % 10
