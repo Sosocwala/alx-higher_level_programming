5cb3887b47659827f1ee2b6103f34cb6d2a6522e
Sonwabile <sonwabilecwala6@gmail.com>
2023-03-14 14:48:54 -0700

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from sys import argv
-add = 0
-for s in argv[1:]:
-    add += int(s)
-print("{:d}".format(add))
+
+if __name__ == "__main__":
+    """Print the addition of all arguments."""
+    import sys
+
